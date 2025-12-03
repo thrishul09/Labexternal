1d06ff30334daf555d7ec6113031b75e2cb75c63
BodhankarAditya-0612 <anushkareddyd@gmail.com>
2025-12-03 10:22:09 +0530

    master commit 2

--- src/main/java/web522/LabExternal/App.java
+++ src/main/java/web522/LabExternal/App.java
@@ -6,7 +6,10 @@ package web522.LabExternal;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+<<<<<<< HEAD
         System.out.println("change 1");
         System.out.println("change 2");
+=======
+>>>>>>> parent of 11ddae2 (b1 commit)
     }
 }
